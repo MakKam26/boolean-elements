add187bcde290db67e943634568d026a9bc75743
MakKam26 <kamzra@googlemail.com>
2024-01-31 11:45:03 +0000

    first commit

---
+++ booleanElements.java
@@ -0,0 +1,34 @@
+public class booleanElements{
+
+public static void main(String[] args){
+
+    boolean[] arrayBooleanExample = new boolean [6];
+
+    arrayBooleanExample[0] = true;
+    arrayBooleanExample[1] = false;
+    arrayBooleanExample[2] = true;
+    arrayBooleanExample[3] = false;
+    arrayBooleanExample[4] = true;
+    arrayBooleanExample[5] = false;
+
+
+
+System.out.println(arrayBooleanExample[5]);
+    
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
